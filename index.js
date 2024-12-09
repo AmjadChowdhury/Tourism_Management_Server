6a8b60c51f192dca6b1c356b847b3671fb75cfb6
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-12-10 01:27:41 +0600

    Delete method introduce

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ async function run() {
       const result = await spotCollection.updateOne(filter,updatedSpot,options)
       res.send(result)
     })
+    app.delete('/spot/:id',async(req,res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const result = await spotCollection.deleteOne(filter)
+      res.send(result)
+    })
 
 
 
